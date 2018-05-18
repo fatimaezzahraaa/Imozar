b4872a1608d254fa4f87eca9c68226e7dfb02ee6
Rofa <enizarboy@gmail.com>
2018-05-18 20:52:27 +0100

    Update
    - Logo is now scrollable on click

--- js/scroll.js
+++ js/scroll.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
         e.preventDefault();
         $('body, html').animate({
             scrollTop: $(this.hash).offset().top
-        }, 1000)
+        }, 1500)
     });
 
     // Active link switch
